52d70f83f54bc6c4b2630e38312a186b25128cb2
Jaro Habiger <jarohabiger@googlemail.com>
2021-04-20 12:12:02 +0200

    more input handling; add counter demo

--- narui_derive/src/lib.rs
+++ narui_derive/src/lib.rs
@@ -4,7 +4,8 @@ mod widget;
 use quote::quote;
 use syn::parse_macro_input;
 
-use syn::{parse_quote, ExprCall};
+use quote::ToTokens;
+use syn::{parse_quote, spanned::Spanned, ExprCall};
 
 #[proc_macro]
 pub fn rsx(input: proc_macro::TokenStream) -> proc_macro::TokenStream { rsx::rsx(input) }
@@ -30,6 +31,39 @@ pub fn widget(
 #[proc_macro]
 pub fn hook(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let mut parsed: ExprCall = parse_macro_input!(input);
-    parsed.args.push(parse_quote! {__context.clone()});
+    let span = parsed.func.span();
+    let key_string =
+        format!("{}@{}:{}", parsed.func.to_token_stream(), span.start().line, span.start().column);
+    parsed.args.push(parse_quote! {__context.enter_hook(#key_string)});
     (quote! {#parsed}).into()
+}
+
+#[proc_macro]
+pub fn color(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let string = input.to_string();
+    let trimmed = string.trim_start_matches("# ");
+    let str_to_float =
+        |s| i64::from_str_radix(s, 16).unwrap() as f32 / 16u32.pow(s.len() as u32) as f32;
+    match trimmed.len() {
+        6 => {
+            let r = str_to_float(&trimmed[0..2]);
+            let g = str_to_float(&trimmed[2..4]);
+            let b = str_to_float(&trimmed[4..6]);
+            (quote! {
+                Color {
+                    color: palette::rgb::Rgb {
+                        red: #r,
+                        green: #g,
+                        blue: #b,
+                        standard: core::marker::PhantomData,
+                    },
+                    alpha: 1.0
+                }
+            })
+            .into()
+        }
+        _ => {
+            unimplemented!()
+        }
+    }
 }
