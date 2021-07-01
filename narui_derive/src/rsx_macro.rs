b854ddc7a3bb5800813934c1acbf508a407e75a5
Jaro Habiger <jarohabiger@googlemail.com>
2021-07-02 00:14:27 +0200

    working version

--- narui_derive/src/rsx_macro.rs
+++ narui_derive/src/rsx_macro.rs
@@ -1,11 +1,11 @@
-use proc_macro2::{Ident, Span};
+use proc_macro2::{Ident, Span, TokenStream};
 use quote::quote;
 use syn_rsx::{Node, NodeType};
 use crate::widget_macro::widget;
 
 pub fn rsx(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let parsed = syn_rsx::parse2(input.into()).unwrap();
-    let (begining, inplace) = handle_rsx_nodes(&parsed, Ident::new("__widget", Span::call_site()));
+    let (begining, inplace) = handle_rsx_nodes(&parsed, Ident::new("__widget", Span::call_site()), None);
     let transformed = quote! {{
         #begining
 
@@ -17,11 +17,15 @@ pub fn rsx(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 
 // if attrs or children of rsx constructs are expressions in the form context => value,
 // we transform them into a closure and call these closures
-fn call_context_closure(input: syn::Expr) -> proc_macro2::TokenStream {
+fn call_context_closure(input: syn::Expr) -> TokenStream {
     quote! {#input}
 }
 
-fn handle_rsx_nodes(input: &Vec<Node>, widget_ident: Ident) -> (proc_macro2::TokenStream, proc_macro2::TokenStream) {
+fn handle_rsx_nodes(input: &Vec<Node>, widget_ident: Ident, key: Option<TokenStream>) -> (TokenStream, TokenStream) {
+    let mut outer_key = key;
+    if outer_key.is_none() {
+        assert_eq!(input.len(), 1);
+    }
     if input.iter().all(|x| x.node_type == NodeType::Element) {
         let (beginning, inplace): (Vec<_>, Vec<_>) = input.iter().map(|x| {
             let name = x.name.as_ref().unwrap();
@@ -37,7 +41,7 @@ fn handle_rsx_nodes(input: &Vec<Node>, widget_ident: Ident) -> (proc_macro2::Tok
                 let name = attribute.name.as_ref().unwrap();
                 let value = call_context_closure(attribute.value.as_ref().unwrap().clone());
                 if name.to_string() == "key" {
-                    key = quote! {KeyPart::WidgetKey { name: #name_str, loc: #loc, key: KeyPart::calculate_hash(#value) }}
+                    key = quote! {KeyPart::WidgetKey { name: #name_str, loc: #loc, hash: KeyPart::calculate_hash(#value) }}
                 } else {
                     processed_attributes.push(quote! {#name=#value});
                 }
@@ -45,11 +49,15 @@ fn handle_rsx_nodes(input: &Vec<Node>, widget_ident: Ident) -> (proc_macro2::Tok
             let (beginning, children_processed) = if x.children.is_empty() {
                 (quote! {}, quote! {})
             } else {
-                let (begining, inplace) = handle_rsx_nodes(&x.children, widget_ident);
+                let (begining, inplace) = handle_rsx_nodes(&x.children, widget_ident, Some(key.clone()));
                 (begining, quote! {
                     children=#inplace,
                 })
             };
+
+            if outer_key.is_none() {
+                outer_key = Some(key.clone());
+            }
 
             (beginning, quote! {(
                 #key,
@@ -63,6 +71,7 @@ fn handle_rsx_nodes(input: &Vec<Node>, widget_ident: Ident) -> (proc_macro2::Tok
             #(#beginning)*
 
             let #widget_ident = Widget {
+                key_part: #outer_key,
                 children: vec![#(#inplace,)*],
                 layout_object: None,
             };
